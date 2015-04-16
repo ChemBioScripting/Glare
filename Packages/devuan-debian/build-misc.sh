480615fe71598ace50a0dbe171d095df3d6561de
sixsixfive <punx665@gmail.com>
2015-04-16 19:23:26 +0200

    fix

--- Packages/devuan-debian/build-misc.sh
+++ Packages/devuan-debian/build-misc.sh
@@ -59,9 +59,9 @@ build(){
 	#ln -s ../themes/Glare-Misc/wppatterns/Glare_pattern_trans-01.png
 	#ln -s ../themes/Glare-Misc/wppatterns/Glare_pattern_trans-01.png.desktop
 #aurorae
-	mkdir -p "$filedir"/usr/share/kde4/aurorae/themes
-	cd "$filedir"/usr/share/kde4/aurorae/themes
-	ln -s ../../../themes/Glare-Misc/Aurorae/Glare
+	mkdir -p "$filedir"/usr/share/kde4/apps/aurorae/themes
+	cd "$filedir"/usr/share/kde4/apps/aurorae/themes
+	ln -s ../../../../themes/Glare-Misc/Aurorae/Glare
 ##configs
 #compton
 	mkdir -p "$filedir"/etc/xdg
